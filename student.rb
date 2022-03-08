7caf9a5048307e6372871e9f0c84af0d3410cc7a
vic778 <victoiremmanuelbarh@gmail.com>
2022-03-08 10:02:11 +0200

    Creat an inherits

--- student.rb
+++ student.rb
@@ -1,17 +1,20 @@
-require './person.rb'
+require_relative './person'
 
 class Student < Person
-  attr_reader :classroom
-  def initialize(age, name = "Unknown", parent_permission = true)
-    super(age, name, parent_permission)
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: true)
+    @age = age
+    @name = name
+    @classroom = classroom
+    @parent_permission = parent_permission
   end
 
   def play_hooky
     "¯\(ツ)/¯"
   end
-
-  def classroom=(classroom)
-    @classroom = classroom
-    classroom.students.push(self) unless classroom.students.include?(self)
-  end
 end
+
+student = Student.new('class1', 24, 'barh', parent_permission: false)
+
+puts student.name
+puts student.can_use_services?
--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,18 @@
-require './person.rb'
+require_relative './person'
 
 class Teacher < Person
-  def initialize(age, specialization, name = "Unknown", parent_permission = true)
-    super(age, name, parent_permission)
+  def initialize(name = 'Unknown', age, specialization, parent_permission: true)
+    super(age, name, parent_permission: true)
     @specialization = specialization
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
   end
 
   def can_use_services?
     true
   end
 end
+
+teacher = Teacher.new('victor', 10, 'programming', parent_permission: false)
+puts teacher.can_use_services?
